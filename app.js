9a02a1edc8db81f93008e9f6de3eef5fd51bb5ba
meresa <meresagidey0938@gmail.com>
2020-09-30 14:01:00 +0100

    commit

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose=require('mongoose');
-var bodyParser=require('body-parser');
-const config=require('./config/dev.config')
-const cors=require('cors');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const config = require('./config/dev.config')
+const cors = require('cors');
 
 
 
@@ -22,54 +22,53 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors({origin: 'http://localhost:4200'}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({ origin: 'http://localhost:4200' }));
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE");
-      return res.status(200).json({});
-  }
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE");
+        return res.status(200).json({});
+    }
+    next();
 });
 require('./routes/index')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const error=new Error('please check the path, it is not the right path.')
-  error.status=404
-  next(error);
+    const error = new Error('please check the path, it is not the right path.')
+    error.status = 404
+    next(error);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
-mongoose.connect(config.database, 
-  { 
-    useUnifiedTopology: true, 
-    useNewUrlParser:true,
-    useCreateIndex: true,
-    useFindAndModify:false
-  } ,
-  function (err) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('successfully connected to db'); /**/
-  }
-});
+mongoose.connect(process.env.MONGODB_URI || config.database, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('successfully connected to db'); /**/
+        }
+    });
 
 module.exports = app;
