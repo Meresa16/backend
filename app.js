d35f18810d70375bf6c6ab073124c6123a674ad3
meresa <meresagidey0938@gmail.com>
2020-10-06 09:22:27 +0100

    commieted ones

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ const cors = require('cors');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+/* app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade'); */
+app.use(express.static(__dirname + '/dist'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({ origin: 'http://localhost:4200' }));
